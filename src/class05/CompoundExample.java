f30a49529b61e34f08e61c1bdcc90f40c03cbb73
ChNurpeyil <nurpeiil.ashibaeva.ch@gmail.com>
2023-03-06 18:50:46 +0600

    compound

---
+++ src/class05/CompoundExample.java
@@ -0,0 +1,29 @@
+package class05;
+
+public class IncrementDecrementExample {
+    public static void main(String[] args) {
+        int num1 = 10;
+        num1+=5;
+        System.out.println(num1);
+
+        int num2 = 11;
+        num2+=22;
+        System.out.println(num2);//33
+
+        num2+=7;
+        System.out.println(num2);//40
+
+        num2-=10;
+        System.out.println(num2);//30
+
+        System.out.println(num2 - 5);//(25)
+        System.out.println(num2-6);//(26)
+
+        num2+=5;
+        System.out.println(num2);//35
+
+
+
+
+    }
+}
--- src/class05/IncrementDecrementExample.java
+++
@@ -1,2 +0,0 @@
-package class05;public class IncrementDecrementExample {
-}
